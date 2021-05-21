1227dc8d6afd15adb4bd528fc86bd88216064d20
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-05-21 11:04:53 -0700

    Beginning JS practices

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/Main.js
@@ -0,0 +1,7 @@
+var A = "Click OK to view this site", smiley = ":)"; //multiple variables
+var blueFont = "This font is blue", blueFont = blueFont.fontcolor("blue"); //changing the color of font
+
+//This is the pop up window seen when first visiting the website
+window.alert(A)
+
+document.write(blueFont + " and Seven multiplied by Three returns a value of " + 7*3 + ". " + smiley);
