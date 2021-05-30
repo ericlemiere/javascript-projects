071b7f2e40b13c97331ba5a1351c97fe51eca2cc
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2021-05-29 21:32:23 -0700

    Tic Tac Toe updated and running

--- TicTacToe/JS/TicTacToe.js
+++ TicTacToe/JS/TicTacToe.js
@@ -14,29 +14,40 @@ function placeXorO(squareNumber) {
         if (activePlayer === "X") {
             select.style.backgroundImage = 'url("images/neonx.png")';
             //active player can only be X or O, so if not X it must be O
+            //placement sound
+            audio('./media/kick.wav');
         }
         else {
             select.style.backgroundImage = 'url("images/neono.png")';
+            //placement sound
+            audio('./media/snare.wav');
         }
         //square number and active player are concatenated together and added to array
         selectedSquares.push(squareNumber + activePlayer);
         
         checkWinConditions();
+        console.log(checkWinConditions());
 
         if (activePlayer === "X") {
             activePlayer = "O";
+            if(checkWinConditions() == false) {
+                document.getElementById("gameDisplay").innerHTML = "O's turn!";
+            }
         }
         else {
             activePlayer = "X";
+            if(checkWinConditions() == false) {
+                document.getElementById("gameDisplay").innerHTML = "X's turn!";
+            }
+            
         }
 
-        //placement sound
-        audio('./media/rattlehit.wav');
+        
 
         //condition checks to see if it is computer's turn
         if(activePlayer === "O") {
             disableClick();
-            setTimeout(function (){ computersTurn(); }, 1000);
+            setTimeout(function (){ computersTurn(); }, 2000);
         }
         return true; //true is needed for computersTurn() to work
     }
@@ -47,7 +58,7 @@ function placeXorO(squareNumber) {
     function computersTurn() {
         let success = false;
         let pickASquare;
-        while (!success) {
+        while (!success && checkWinConditions() == false) {
             //assigns random number between 0-8
             pickASquare = String(Math.floor(Math.random()*9));
             //if random number evaluated returns true, the square is empty
@@ -65,57 +76,76 @@ function placeXorO(squareNumber) {
 function checkWinConditions() {
     if (arrayIncludes("0X", "1X", "2X")) {
         drawWinLine(50, 100, 558, 100);
+        return true;
     }
     else if (arrayIncludes("3X", "4X", "5X")) {
         drawWinLine(50, 304, 558, 304);
+        return true;
     }
     else if (arrayIncludes("6X", "7X", "8X")) {
         drawWinLine(50, 508, 558, 508);
+        return true;
     }
     else if (arrayIncludes("0X", "3X", "6X")) {
         drawWinLine(100, 50, 100, 558);
+        return true;
     }
     else if (arrayIncludes("1X", "4X", "7X")) {
         drawWinLine(304, 100, 304, 558);
+        return true;
     }
     else if (arrayIncludes("2X", "5X", "8X")) {
         drawWinLine(508, 50, 508, 558);
+        return true;
     }
     else if (arrayIncludes("6X", "4X", "2X")) {
         drawWinLine(100, 508, 510, 90);
+        return true;
     }
     else if (arrayIncludes("0X", "4X", "8X")) {
         drawWinLine(100, 100, 520, 520);
+        return true;
     }
     else if (arrayIncludes("0O", "1O", "2O")) {
         drawWinLine(50, 100, 558, 100);
+        return true;
     }
     else if (arrayIncludes("3O", "4O", "5O")) {
         drawWinLine(50, 304, 558, 304);
+        return true;
     }
     else if (arrayIncludes("6O", "7O", "8O")) {
         drawWinLine(50, 508, 558, 508);
+        return true;
     }
     else if (arrayIncludes("0O", "3O", "6O")) {
         drawWinLine(100, 50, 100, 558);
+        return true;
     }
     else if (arrayIncludes("1O", "4O", "7O")) {
         drawWinLine(304, 50, 304, 558);
+        return true;
     }
     else if (arrayIncludes("2O", "5O", "8O")) {
         drawWinLine(508, 50, 508, 558);
+        return true;
     }
     else if (arrayIncludes("6O", "4O", "2O")) {
         drawWinLine(100, 508, 510, 90);
+        return true;
     }
     else if (arrayIncludes("0O", "4O", "8O")) {
         drawWinLine(100, 100, 520, 520);
+        return true;
     }
     else if (selectedSquares.length >= 9) {
-        audio('./media/swell.wav');
-        setTimeout(function () { resetGame(); }, 4000);
-        //setTimeout(function () { alert("TIE GAME!\nPress OK to play again");}, 1500);
+        audio('./media/snap.wav');
+        document.getElementById("gameDisplay").innerHTML = "TIE GAME! Play again?";
+        setTimeout(function () { resetGame(); }, 2000);
+        return true;
+        
     }
+    else { return false; }
 
     //Function checks if an array inclues 3 strings and is used for win conditions
     function arrayIncludes(squareA, squareB, squareC) {
@@ -182,10 +212,10 @@ function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
         cancelAnimationFrame(animationLoop);
     }
     disableClick();
-    audio('./media/beatzilla.wav');
+    audio('./media/winner.wav');
     animateLineDrawing();
-    setTimeout(function () { clear(); resetGame(); }, 8000);
-    //setTimeout(function () { alert("Press OK to play again");}, 2000);
+    document.getElementById("gameDisplay").innerHTML = "WE HAVE A WINNER!";
+    setTimeout(function () { clear(); resetGame(); }, 2000);
 }
 
 
@@ -196,4 +226,5 @@ function resetGame() {
         square.style.backgroundImage = "";
     }
     selectedSquares = [];
+
 }
